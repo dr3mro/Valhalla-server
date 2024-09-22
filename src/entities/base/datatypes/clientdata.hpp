410862cc2e5c0768aee9e251f5d18596c282b1df
Dr. Amr Osman <dr3mro@gmail.com>
2024-09-22 14:58:26 +0300

    remove fullname validation regexp

--- src/entities/base/datatypes/clientdata.hpp
+++ src/entities/base/datatypes/clientdata.hpp
@@ -24,7 +24,7 @@ class ClientData
     const std::map<std::string, std::string>         validators    = {
         {"username", "^[a-z][a-z0-9_]*$"},
         {"password", "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$"},
-        {"fullname", "^[A-Z][a-zA-Z]*([ '-][A-Z][a-zA-Z]*)*$"},
+        //{"fullname", "^[A-Z][a-zA-Z]*([ '-][A-Z][a-zA-Z]*)*$"},
         {"phone", R"(^\+?(\d{1,3})?[-.\s]?(\(?\d{3}\)?)?[-.\s]?\d{3}[-.\s]?\d{4}$)"},
         {"email", R"((\w+)(\.\w+)*@(\w+)(\.\w+)+)"},
         {"dob", R"(^(0[1-9]|[12]\d|3[01])-(0[1-9]|1[0-2])-\d{4}$)"},
