49b170a1a86650dbf9df5e5f0602ea101f4a93e8
Dr. Amr Osman <dr3mro@gmail.com>
2024-10-01 14:53:11 +0300

    make errors and failure messages always return as json

--- src/utils/resthelper/resthelper.cpp
+++ src/utils/resthelper/resthelper.cpp
@@ -5,20 +5,34 @@
 void RestHelper::failureResponse(crow::response& res, const std::string& status_message)
 {
     res.code = crow::status::INTERNAL_SERVER_ERROR;
-    res.add_header("msg", "fail");
-    res.end(fmt::format("Error: {}", status_message));
+    res.add_header("status", "failure");
+
+    jsoncons::json error_json;
+    std::string    response;
+
+    error_json["message"] = status_message;
+    error_json.dump_pretty(response);
+
+    res.end(response);
 }
 
 void RestHelper::errorResponse(crow::response& res, const crow::status& status, const std::string& status_message)
 {
     res.code = status;
-    res.add_header("msg", "fail");
-    res.end(fmt::format("Error: {}", status_message));
+    res.add_header("status", "error");
+
+    jsoncons::json error_json;
+    std::string    response;
+
+    error_json["message"] = status_message;
+    error_json.dump_pretty(response);
+
+    res.end(response);
 }
 
 void RestHelper::successResponse(crow::response& res, const crow::status& status, const std::string& result)
 {
     res.code = status;
-    res.add_header("msg", "success");
+    res.add_header("status", "success");
     res.end(result);
 }
