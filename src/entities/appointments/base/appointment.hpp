bb3eb489ae6f39c9ce4fd188c6fbf33715b6b93c
Dr. Amr Osman <dr3mro@gmail.com>
2024-10-29 17:04:23 +0300

    remove in JSON search code and make all entities search in SQL type

--- src/entities/appointments/base/appointment.hpp
+++ src/entities/appointments/base/appointment.hpp
@@ -1,9 +1,5 @@
 #pragma once
-
-#include <any>
-
 #include "entities/base/entity.hpp"
-#include "fmt/format.h"
 #include "jsoncons/basic_json.hpp"
 using json = jsoncons::json;
 
@@ -18,23 +14,4 @@ class Appointment : public Entity
     Appointment(const std::string &_tablename) : Entity(_tablename) {}
 
     ~Appointment() = default;
-
-    std::optional<std::string> getSqlSearchStatement() final
-    {
-        std::optional<std::string> query;
-        try
-        {
-            SearchData searchdata = std::any_cast<SearchData>(getData());
-            query = fmt::format("SELECT * FROM {}_safe WHERE {} = {} ORDER BY {} {} LIMIT {} OFFSET {};", tablename, searchdata.filter,
-                                searchdata.keyword, searchdata.order_by, searchdata.direction, searchdata.limit + 1, searchdata.offset);
-            std::cout << query.value() << '\n';
-        }
-        catch (const std::exception &e)
-        {
-            std::cerr << "faild to create query for search " << tablename << e.what() << '\n';
-            return std::nullopt;
-        }
-
-        return query;
-    }
 };
