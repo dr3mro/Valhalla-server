fcc7949facc541968b20083d2c7c0f3ad9439475
Dr. Amr Osman <dr3mro@gmail.com>
2024-11-07 14:44:22 +0200

    reverse last changes

--- src/entities/base/entity.hpp
+++ src/entities/base/entity.hpp
@@ -18,83 +18,12 @@ using json = jsoncons::json;
 class Entity : public Base
 {
    public:
-    struct ClientData
+    using Create_t = struct Create_t
     {
        public:
-        ClientData(const json &data, const uint64_t id) : data_j(data), id(id) {}
-        ClientData(const json &data, crow::response &res, bool &success) : data_j(data)  // clients
-        {
-            for (const auto &item : data.object_range())
-            {
-                try
-                {
-                    std::optional<std::string> value = item.value().as<std::string>();
-                    if (value.has_value() && !value->empty())
-                    {
-                        auto pattern_item =
-                            std::find_if(validators.begin(), validators.end(), [&](const auto &validator) { return validator.first == item.key(); });
-
-                        if (pattern_item != validators.end())
-                        {
-                            std::regex pattern(pattern_item->second);
-                            if (!std::regex_match(value.value(), pattern))
-                            {
-                                throw std::runtime_error(fmt::format("Value({}) is invalid.", value.value(), item.key()));
-                            }
-                        }
-
-                        if (item.key() == "password")
-                        {
-                            value              = passwordCrypt->hashPassword(value.value());
-                            data_j["password"] = value;
-                        }
-
-                        db_data.push_back({item.key(), value.value()});
-                    }
-                }
-                catch (const std::exception &e)
-                {
-                    RestHelper::failureResponse(res, e.what());
-                    return;
-                }
-            }
-            success = true;
-        }
-        const std::vector<std::pair<std::string, std::string>> &get_data() const { return db_data; }
-        jsoncons::json                                          data_j;
-        uint64_t                                                id;
-
-       protected:
-        std::shared_ptr<PasswordCrypt> passwordCrypt = Store::getObject<PasswordCrypt>();
-
-       private:
-        std::vector<std::pair<std::string, std::string>> db_data;
-
-        const std::map<std::string, std::string> validators = {
-            {"username", "^[a-z][a-z0-9_]*$"},
-            {"password", "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$"},
-            {"phone", R"(^\+?(\d{1,3})?[-.\s]?(\(?\d{3}\)?)?[-.\s]?\d{3}[-.\s]?\d{4}$)"},
-            {"email", R"((\w+)(\.\w+)*@(\w+)(\.\w+)+)"},
-            {"dob", R"(^(0[1-9]|[12]\d|3[01])-(0[1-9]|1[0-2])-\d{4}$)"},
-            {"gender", "^(male|female)$"},
-        };
-    };
-
-    using Create_t = struct Create_t : public ClientData
-    {
-       public:
-        Create_t(const json &data, const uint64_t id) : ClientData(data, id) {}
-        Create_t(const json &data, crow::response &res, bool &success) : ClientData(data, res, success) {};
-    };
-
-    using Update_t = struct Update_t : public ClientData
-    {
-       public:
-        Update_t(const json &data, const uint64_t id) : ClientData(data, id) {}
-        Update_t(const json &data, crow::response &res, bool &success)
-            : ClientData(data, res, success) {
-
-              };
+        jsoncons::json data_j;
+        uint64_t       id;
+        Create_t(const json &data, const uint64_t id) : data_j(data), id(id) {}
     };
 
     using Read_t = struct Read_t
@@ -102,6 +31,14 @@ class Entity : public Base
         std::vector<std::string> schema;
         uint64_t                 id;
         Read_t(const std::vector<std::string> &_schema, const uint64_t _id) : schema(_schema), id(_id) {}
+    };
+
+    using Update_t = struct Update_t
+    {
+       public:
+        jsoncons::json data_j;
+        uint64_t       id;
+        Update_t(const json &data, const uint64_t id) : data_j(data), id(id) {}
     };
 
     using Delete_t = struct Delete_t
