8b37a8053c6557017f17d822ec8b93e6fb22dadd
Dr. Amr Osman <dr3mro@gmail.com>
2024-11-15 21:45:33 +0200

    update

--- src/configurator/defaults.hpp
+++ src/configurator/defaults.hpp
@@ -45,7 +45,7 @@ namespace Defaults
         constexpr std::string_view SERVER_VER_            = std::string_view("1.0.0");
         constexpr std::string_view SERVER_DESC_           = std::string_view("ProjectValhalla API");
         constexpr std::string_view SERVER_HOST_           = std::string_view("0.0.0.0");
-        constexpr uint8_t          SERVER_DEBUG_LEVEL_    = 3;
+        constexpr uint8_t          SERVER_DEBUG_LEVEL_    = 1;
         constexpr bool             SERVER_LOG_TO_CONSOLE_ = true;
         constexpr bool             SERVER_LOG_TO_FILE_    = true;
         constexpr std::string_view SERVER_LOG_DIR_        = std::string_view("./build/");
