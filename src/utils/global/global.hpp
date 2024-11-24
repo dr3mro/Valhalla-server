4e015795f3a4cf08adf5899abbb97d2766631a67
Dr. Amr Osman <dr3mro@gmail.com>
2024-11-24 02:38:13 +0200

    dummy push

--- src/utils/global/global.hpp
+++ src/utils/global/global.hpp
@@ -1,6 +1,7 @@
 #define CRITICALMESSAGE                                                                                             \
     std::string critical_error = fmt::format("Failure: {} : {} : {} : {}", e.what(), __func__, __FILE__, __LINE__); \
     Message::CriticalMessage(critical_error);
+
 #define CRITICALMESSAGERESPONSE \
     CRITICALMESSAGE             \
-    callback(500, critical_error);
+    callback(500, critical_error);\
