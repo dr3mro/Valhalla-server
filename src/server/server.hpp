9bcd9ff3bdce8c57c52f672eb43a0b21a75ceba8
Dr. Amr Osman <dr3mro@gmail.com>
2024-12-02 01:25:20 +0200

    fix logger init

--- src/server/server.hpp
+++ src/server/server.hpp
@@ -8,6 +8,7 @@
 #include "api/v2/middlewares/elapsedtime.hpp"
 #include "configurator/configurator.hpp"
 #include "store/store.hpp"
+#include "utils/Logger/logger.hpp"
 #include "validator/databaseschema/databaseschema.hpp"
 class Server
 {
@@ -18,6 +19,7 @@ class Server
     int run();
 
    private:
+    std::shared_ptr<Logger>                            logger_       = Store::getObject<Logger>();
     std::shared_ptr<Configurator>                      configurator_ = Store::getObject<Configurator>();
     const Configurator::ServerConfig                  &config_       = configurator_->get<Configurator::ServerConfig>();
     const Configurator::DatabaseConfig                &db_config_    = configurator_->get<Configurator::DatabaseConfig>();
