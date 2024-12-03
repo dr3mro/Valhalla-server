b62d4eb084034cb3dc027af5b6096fb7e0a88ef8
Dr. Amr Osman <dr3mro@gmail.com>
2024-12-04 00:07:08 +0200

    remove conan profile

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-conan install . --build=missing -s build_type=Release --profile:build=gcc142R
-conan install . --build=missing -s build_type=Debug --profile:build=gcc142D
+conan install . --build=missing -s build_type=Release 
+conan install . --build=missing -s build_type=Debug 
 cmake -S. -Bbuild/Release -DCMAKE_BUILD_TYPE=Release
 cmake -S. -Bbuild/Debug -DCMAKE_BUILD_TYPE=Debug
 cmake --build build/Release
