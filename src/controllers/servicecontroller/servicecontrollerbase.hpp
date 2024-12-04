c9507506f695f373badb35a045cdb6f21de99cc8
Dr. Amr Osman <dr3mro@gmail.com>
2024-12-04 02:51:36 +0200

    add some missing headers

--- src/controllers/servicecontroller/servicecontrollerbase.hpp
+++ src/controllers/servicecontroller/servicecontrollerbase.hpp
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <cstdint>
+#include <functional>
 #include <string_view>
 
 #include "utils/global/callback.hpp"
--- src/controllers/staffcontroller/staffcontrollerBase.hpp
+++ src/controllers/staffcontroller/staffcontrollerBase.hpp
@@ -1,6 +1,7 @@
 
 #pragma once
 
+#include <functional>
 #include <string_view>
 
 #include "utils/global/callback.hpp"
