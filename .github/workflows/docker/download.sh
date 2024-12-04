03faf2fca12743da082b979c72e7c64f9a02c899
Dr. Amr Osman <dr3mro@gmail.com>
2024-12-05 00:31:27 +0200

    style fix

--- .github/workflows/docker/download.sh
+++ .github/workflows/docker/download.sh
@@ -32,24 +32,24 @@ GITHUB="https://api.github.com"
 alias errcho='>&2 echo'
 
 function gh_curl() {
-	curl -H "Authorization: token $TOKEN" \
+	curl -H "Authorization: token ${TOKEN}" \
 		-H "Accept: application/vnd.github.v3.raw" \
 		$@
 }
 
-if [ "$VERSION" = "latest" ]; then
+if [[ ${VERSION} == "latest" ]]; then
 	# Github should return the latest release first.
-	parser=".[0].assets | map(select(.name == \"$FILE\"))[0].id"
+	parser=".[0].assets | map(select(.name == \"${FILE}\"))[0].id"
 else
-	parser=". | map(select(.tag_name == \"$VERSION\"))[0].assets | map(select(.name == \"$FILE\"))[0].id"
+	parser=". | map(select(.tag_name == \"${VERSION}\"))[0].assets | map(select(.name == \"${FILE}\"))[0].id"
 fi
 
-asset_id=$(gh_curl -s $GITHUB/repos/$REPO/releases | jq "$parser")
-if [ "$asset_id" = "null" ]; then
-	errcho "ERROR: version not found $VERSION"
+asset_id=$(gh_curl -s "${GITHUB}"/repos/"${REPO}"/releases | jq "${parser}")
+if [[ ${asset_id} == "null" ]]; then
+	errcho "ERROR: version not found ${VERSION}"
 	exit 1
 fi
 
 wget -q --auth-no-challenge --header='Accept:application/octet-stream' \
-	https://$TOKEN:@api.github.com/repos/$REPO/releases/assets/$asset_id \
-	-O $FILE
+	https://"${TOKEN}":@api.github.com/repos/"${REPO}"/releases/assets/"${asset_id}" \
+	-O "${FILE}"
--- src/gatekeeper/dosdetector/dosdetector.hpp
+++ src/gatekeeper/dosdetector/dosdetector.hpp
@@ -38,9 +38,9 @@ class DOSDetector
     DOSDetector::Status is_dos_attack(const Request &request);
 
    private:
-    std::shared_ptr<Configurator>            configurator_       = Store::getObject<Configurator>();
-    const Configurator::DOSDetectorConfig   &config_             = configurator_->get<Configurator::DOSDetectorConfig>();
-    static constexpr int                     REQUEST_BUFFER_SIZE = 4096;
+    std::shared_ptr<Configurator>          configurator_       = Store::getObject<Configurator>();
+    const Configurator::DOSDetectorConfig &config_             = configurator_->get<Configurator::DOSDetectorConfig>();
+    static constexpr int                   REQUEST_BUFFER_SIZE = 4096;
     inline void __attribute((always_inline)) clean_requests(const std::chrono::time_point<std::chrono::steady_clock> &window);
     inline void __attribute((always_inline)) clean_ratelimited_ips(const std::chrono::time_point<std::chrono::steady_clock> &now);
     inline void __attribute((always_inline)) clean_banned_ips(const std::chrono::time_point<std::chrono::steady_clock> &now);
@@ -62,10 +62,10 @@ class DOSDetector
     std::future<void> async_task_clean_;
     std::atomic<bool> running_clean_{true};
 
-    void                                                           cleanUpTask();
+    void cleanUpTask();
     inline std::optional<std::string> __attribute((always_inline)) generateRequestFingerprint(const DOSDetector::Request &request);
-    inline bool __attribute((always_inline))                       isWhitelisted(std::string_view remote_ip);
-    inline bool __attribute((always_inline))                       isBlacklisted(std::string_view remote_ip);
+    inline bool __attribute((always_inline)) isWhitelisted(std::string_view remote_ip);
+    inline bool __attribute((always_inline)) isBlacklisted(std::string_view remote_ip);
     inline bool __attribute((always_inline)) regexFind(std::string_view remote_ip, const std::unordered_set<std::string> &list, std::mutex &mtx);
     inline bool __attribute((always_inline)) isBanned(std::string_view remote_ip);
     inline bool __attribute((always_inline)) isRateLimited(std::string_view remote_ip);
