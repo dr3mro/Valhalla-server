71e19702e98eff839a193a8bcb35041b70c9ae34
Dr. Amr Osman <dr3mro@gmail.com>
2024-12-22 03:43:49 +0200

    code improvement

--- src/utils/global/requester.hpp
+++ src/utils/global/requester.hpp
@@ -11,15 +11,35 @@ namespace api::v2
         std::string group;
 
        public:
+        enum class GroupId : uint8_t
+        {
+            Users,
+            Providers,
+            Owners,
+            Admin,
+            Super,
+            Staff
+        };
+
         explicit Requester(uint64_t _id, std::string _group) : id(_id), group(std::move(_group)) {}
+        Requester() = delete;
+
+        Requester(const Requester &) = default;
+        Requester(Requester &&)      = default;
+
+        Requester &operator=(const Requester &) = default;
+        Requester &operator=(Requester &&)      = default;
+
+        virtual ~Requester() = default;
+
         [[nodiscard]] uint64_t           getId() const { return id; }
         [[nodiscard]] const std::string &getGroup() const { return group; }
-        [[nodiscard]] bool               isAdmin() const { return group == "admins"; }
-        [[nodiscard]] bool               isUser() const { return group == "users"; }
-        [[nodiscard]] bool               isProvider() const { return group == "providers"; }
-        [[nodiscard]] bool               isStaff() const { return group == "staff"; }
-        [[nodiscard]] bool               isOwner() const { return group == "owners"; }
-        [[nodiscard]] bool               isSuper() const { return group == "Super"; }
+        [[nodiscard]] bool               isAdmin() const { return groupMatch("admins"); }
+        [[nodiscard]] bool               isUser() const { return groupMatch("users"); }
+        [[nodiscard]] bool               isProvider() const { return groupMatch("providers"); }
+        [[nodiscard]] bool               isStaff() const { return groupMatch("staff"); }
+        [[nodiscard]] bool               isOwner() const { return groupMatch("owners"); }
+        [[nodiscard]] bool               isSuper() const { return groupMatch("Super"); }
         [[nodiscard]] bool               groupMatch(const std::string &_group) const { return this->group == _group; }
         [[nodiscard]] bool               idMatch(uint64_t _id) const { return this->id == _id; }
         void                             setId(uint64_t _id) { this->id = _id; }
