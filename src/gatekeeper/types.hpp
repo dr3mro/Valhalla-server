2c47e92d5c665380d918cb91c4890348df6c48de
Dr. Amr Osman <dr3mro@gmail.com>
2024-12-22 17:02:12 +0200

    rev

--- src/gatekeeper/types.hpp
+++ src/gatekeeper/types.hpp
@@ -1,33 +1,29 @@
 #pragma once
+#include <chrono>
+#include <cstdint>
 #include <optional>
 #include <string>
 
-namespace api
+namespace api::v2::Types
 {
-    namespace v2
+    using ClientLoginData = struct ClientLoginData
     {
-        namespace Types
-        {
-            using ClientLoginData = struct ClientLoginData
-            {
-                std::optional<std::string>          token;
-                std::optional<std::string>          username;
-                std::optional<std::string>          group;
-                std::optional<uint64_t>             clientId;
-                std::optional<std::string>          lastLogoutTime;
-                std::optional<std::string>          nowLoginTime;
-                bool                                is_active;
-                bool                                passwordMatch;
-                std::optional<std::string>          passwordHash;
-                std::chrono::system_clock::duration expireTime;
-                std::optional<std::string>          ip_address;
-            };
+        std::optional<std::string>          token;
+        std::optional<std::string>          username;
+        std::optional<std::string>          group;
+        std::optional<uint64_t>             clientId;
+        std::optional<std::string>          lastLogoutTime;
+        std::optional<std::string>          nowLoginTime;
+        bool                                is_active;
+        bool                                passwordMatch;
+        std::optional<std::string>          passwordHash;
+        std::chrono::system_clock::duration expireTime;
+        std::optional<std::string>          ip_address;
+    };
 
-            using Credentials = struct Credentials
-            {
-                std::string username;  ///< The user's username.
-                std::string password;  ///< The user's password.
-            };
-        }  // namespace Types
-    }  // namespace v2
-}  // namespace api
+    using Credentials = struct Credentials
+    {
+        std::string username;  ///< The user's username.
+        std::string password;  ///< The user's password.
+    };
+}  // namespace api::v2::Types
