a7a8069d2965b304a9b92d0f17b13caa6f466a0b
Dr. Amr Osman <dr3mro@gmail.com>
2024-12-24 14:37:58 +0200

    minor edit

--- src/server/server.hpp
+++ src/server/server.hpp
@@ -23,12 +23,13 @@ class Server
     int run();
 
    private:
+    void print_banner();
+
     std::shared_ptr<Logger>                            logger_       = Store::getObject<Logger>();
     std::shared_ptr<Configurator>                      configurator_ = Store::getObject<Configurator>();
     Configurator::ServerConfig                         config_       = configurator_->get<Configurator::ServerConfig>();
     Configurator::DatabaseConfig                       db_config_    = configurator_->get<Configurator::DatabaseConfig>();
     std::shared_ptr<DatabaseSchema>                    database      = Store::getObject<DatabaseSchema>();
-    void                                               print_banner();
     std::shared_ptr<api::v2::Filters::Auth>            auth_filter_  = std::make_shared<api::v2::Filters::Auth>();
     std::shared_ptr<api::v2::MiddleWares::ElapsedTime> elapsed_time_ = std::make_shared<api::v2::MiddleWares::ElapsedTime>();
     std::shared_ptr<api::v2::Filters::RateLimit>       rate_limit_   = std::make_shared<api::v2::Filters::RateLimit>();
