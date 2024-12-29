c7b5bc753d54e2da0c17b52b3aac9b1ccdb976e4
Dr. Amr Osman <dr3mro@gmail.com>
2024-12-30 00:30:45 +0200

    init server on heap

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
+#include <memory>
+
 #include "server/server.hpp"
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
 {
-    Server server;
-    return server.run();
+    std::unique_ptr<Server> server = std::make_unique<Server>();
+    return server->run();
 }
